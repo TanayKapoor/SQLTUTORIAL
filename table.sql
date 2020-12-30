326cd940ee59014122bfa54cc100cd49db1fca6e
tanaykapoor <itstkaay@gmail.com>
2020-12-30 13:01:20 +0530

    Added FK

--- table.sql
+++ table.sql
@@ -36,3 +36,37 @@ ALTER COLUMN FirstName VARCHAR(200);
 -- NOT NULL 
 ALTER TABLE Persons
 MODIFY PersonID int NOT NULL;
+
+-- UNIQUE
+ALTER TABLE Persons
+ADD UNIQUE (PersonID);
+
+-- Droping UNIQUE
+ALTER TABLE Persons
+
+-- ADDING PK
+CREATE TABLE Persons2
+(
+    PersonID INT,
+    LastName VARCHAR(255),
+    FirstName VARCHAR(255),
+    Address VARCHAR (255),
+    City VARCHAR (255),
+    Age int,
+    CONSTRAINT Primary_Person PRIMARY KEY (PersonID, LastName)
+);
+
+-- Removing PK
+ALTER TABLE Persons2
+DROP CONSTRAINT Primary_Person(PersonID, LastName);
+
+-- FOREIGN KEY 
+CREATE TABLE Orders (
+    OrderID int NOT NULL,
+    OrderNumber int NOT NULL,
+    PersonID int,
+    PRIMARY KEY (OrderID),
+    FOREIGN KEY (PersonID) REFERENCES Persons(PersonID)
+);
+
+-- CHECK
